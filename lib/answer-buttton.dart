a4256585e0f54c3e62d640e7936d9527d39273d3
Ozy2022 <ozy.almondry2021@gmail.com>
2025-01-17 23:51:27 +0300

    answer-button file, and provide a list of buttons answers as a final type value.

---
+++ lib/answer-buttton.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    //works as a positional argument
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap; // return no value
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
